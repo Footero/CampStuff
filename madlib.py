f5b609181c6cd408a9604c7fab54e0292e315af0
Footero <33110939+Footero@users.noreply.github.com>
2019-07-22 15:06:09 -0600

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,11 @@
+def main():
+    print("Answer the following questions:")
+    name = input("Enter any name: ")
+    pet = input("Enter any animal: ")
+    food = input("Enter your favourite food: ")
+    
+    
+    print(name + " lived on a farm with their favourite pet, Joe the " + pet + ". One day, " + name + "was feeling hungry so they went to Joe the " + pet + "and ate " + food + " with them. The end." )
+
+if __name__ == "__main__":
+    main()
