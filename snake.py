d22bdc5f076b2e6ab749c00b9e7bc78873177b59
Footero <33110939+Footero@users.noreply.github.com>
2019-07-26 14:03:47 -0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,40 +1,48 @@
-# -*- coding: utf-8 -*-
-
-
-
-
-#import modules
-
 import pygame
 
 import random
 
 import time
 
+
+
 def draw_apple(apple_X,apple_Y):
     pygame.draw.rect(display, player_color,pygame.Rect(apple_X, apple_Y,10,10))
+def draw_death(death_X,death_Y):
+    pygame.draw.rect(display, death_color,pygame.Rect(death_X, death_Y,20,20))    
 def generate_apple():
     apple_X = random.randint(2,89) * 10
     apple_Y = random.randint(2,89) * 10
     return apple_X, apple_Y
-def collision_with_apple(snake_head,apple_X,apple_Y,grow):
+def generate_death():
+    death_X = random.randint(4,86) * 10
+    death_Y = random.randint(4,86) * 10
+    return death_X, death_Y
+def collision_with_death(death_X, death_Y):
+    if snake_head[0] == death_X and snake_head[1] == death_Y or snake_head[0] == death_X + 10 and snake_head[1] == death_Y + 10 or snake_head[0] == death_X and snake_head[1] == death_Y + 10 or snake_head[0] == death_X + 10 and snake_head[1] == death_Y:
+        return 1
+    return 0
+def collision_with_apple(snake_head,apple_X,apple_Y,grow,score,speed,death_X,death_Y):
     if snake_head[0] == apple_X and snake_head[1] == apple_Y:
         apple_X, apple_Y = generate_apple()
+        death_X, death_Y = generate_death()
         grow = True
-    return apple_X, apple_Y, grow
+        pygame.display.set_caption("Snake game             Your score is: " + str(score))
+        score += 1
+        speed += 2
+    return apple_X, apple_Y, grow, score, speed, death_X, death_Y
 
 def collision_with_boundaries(snake_head):
 
     # if snake is outside of boundaries snap back
-    if snake_head[0] >= display_width:
-        snake_head[0] = 10
-    if snake_head[0] <= 0:
+    if snake_head[0] > 900:
+        snake_head[0] = -10
+    if snake_head[0] < -10:
         snake_head[0] = 900
-    if snake_head[1] >= display_height:
-        snake_head[1] = 10
-    if snake_head[1] <= 0:
+    if snake_head[1] > 900:
+        snake_head[1] = -10
+    if snake_head[1] < -10:
         snake_head[1] = 900
-        
         
 def collision_with_self(snake_head, snake_position):
     for position in snake_position[1:]:
@@ -64,11 +72,12 @@ def display_snake(snake_position):
     #uses list of snake's positions to display snake
     for position in snake_position:
         pygame.draw.rect(display, player_color, pygame.Rect(position[0],position[1],10,10))
-def play_game(snake_head, snake_position, button_direction):
+def play_game(snake_head, snake_position, button_direction,score,speed):
 
     crashed = False
     grow = False
     apple_X,apple_Y = generate_apple()
+    death_X,death_Y = generate_death()
 
     while crashed is not True:
 
@@ -100,16 +109,19 @@ def play_game(snake_head, snake_position, button_direction):
         display.fill(window_color)
         display_snake(snake_position)
         draw_apple(apple_X,apple_Y)
+        draw_death(death_X,death_Y)
         pygame.display.update()
-        apple_X, apple_Y, grow = collision_with_apple(snake_head, apple_X, apple_Y, grow)
+        apple_X, apple_Y, grow, score, speed, death_X, death_Y = collision_with_apple(snake_head, apple_X, apple_Y, grow, score, speed, death_X, death_Y)
 
         #ends game loop if snake leaves the boundary
-        if collision_with_boundaries(snake_head) == 1:
-            pass
+        if collision_with_death(death_X, death_Y) == 1:
+            crashed = True
         if collision_with_self(snake_head, snake_position) == 1:
-            pass
+            crashed = True
+        if collision_with_boundaries(snake_head) == 1:
+            crashed = True            
 
-        clock.tick(40)
+        clock.tick(speed)
 
 
 
@@ -122,6 +134,8 @@ if __name__ == "__main__":
     display_height = 900
 
     player_color = (255,255,255)
+    
+    death_color = (255,100,100)
 
     window_color = (0,210,220)
 
@@ -134,7 +148,10 @@ if __name__ == "__main__":
     snake_head = [250,250]
 
     snake_position = [[250,250],[240,250],[230,250]]
-
+    
+    score = 1
+    
+    speed = 10
 
 
     #initialize pygame modules    
@@ -149,7 +166,7 @@ if __name__ == "__main__":
 
     display.fill(window_color)
 
-    pygame.display.set_caption("Snake Game")
+    pygame.display.set_caption("Snake Game            Your score is: 0")
 
     pygame.display.update()
 
@@ -157,7 +174,7 @@ if __name__ == "__main__":
 
     #start the game loop
 
-    play_game(snake_head, snake_position, 1)
+    play_game(snake_head, snake_position, 1, score, speed)
 
 
 
